222434cd27c4d2cd3c08e7609d36cf7484b27494
mikolaj <mikolaj.rambiert@gmail.com>
2018-05-02 23:52:07 +0200

    Update to Unity 2017.4.2, moving platform update

--- Assets/Scripts/Engine/TriggerBroadcast.cs
+++ Assets/Scripts/Engine/TriggerBroadcast.cs
@@ -4,11 +4,20 @@
 public class TriggerBroadcast : MonoBehaviour
 {
     public event System.Action<Collider> TriggerEntered;
+    public event System.Action<Collider> TriggerExit;
     void OnTriggerEnter(Collider collision)
     {
         if (TriggerEntered != null)
         {
             TriggerEntered(collision);
+        }
+    }
+
+    void OnTriggerExit(Collider collision)
+    {
+        if (TriggerExit != null)
+        {
+            TriggerExit(collision);
         }
     }
 }
--- Assets/Scripts/Misc/MovingPlatform.cs
+++ Assets/Scripts/Misc/MovingPlatform.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
@@ -7,6 +8,8 @@ public class MovingPlatform : MonoBehaviour
     public float speed;
     public bool rotate;
     public float rotationSpeed;
+    public TriggerBroadcast triggerBroadcast;
+    public float lerpSpeed;
 
     [Range(0, 1f)]
     public float pos;
@@ -14,21 +17,54 @@ public class MovingPlatform : MonoBehaviour
     float calculatedSpeed;
     float calculatedRotationSpeed;
 
+    Dictionary<int, Transform> platformers;
+    Vector3 lastPos;
+
+    float dis;
+    Vector3 dir;
+
     private void Awake()
     {
+        platformers = new Dictionary<int, Transform>();
         curve = GetComponentInChildren<BezierCurve>();
         calculatedSpeed = speed * 0.0016f;
         prevPos = platform.position;
         calculatedRotationSpeed = rotationSpeed * 0.016f;
+        lastPos = platform.position;
+        triggerBroadcast.TriggerEntered += BroadCastTriggerEnter;
+        triggerBroadcast.TriggerExit += BroadCastTriggerExit;
     }
 
-    private void Update()
+    void BroadCastTriggerEnter(Collider other)
+    {
+        if (other.transform.root.gameObject.layer == Layers.Character)
+        {
+            var transform = other.transform.root;
+            transform.parent = platform;
+            platformers.Add(other.GetInstanceID(), transform);
+        }
+    }
+
+    void BroadCastTriggerExit(Collider other)
+    {
+        Debug.Log("Moving platform TriggerExit");
+        if (platformers.ContainsKey(other.GetInstanceID()))
+        {
+            int id = other.GetInstanceID();
+            var transform = platformers[id];
+            transform.parent = null;
+            platformers.Remove(id);
+        }
+    }
+
+    private void FixedUpdate()
     {
         if (pos + calculatedSpeed >= 1) pos -= 1f;
             pos += calculatedSpeed;
         platform.position = Vector3.Lerp(platform.position, curve.GetPointAt(pos), Time.deltaTime * speed);
         if (rotate)
             platform.rotation = Quaternion.Lerp(platform.rotation, Quaternion.LookRotation(Engine.Vector.Direction(platform.position, curve.GetPointAt(Mathf.Clamp01(pos)))), calculatedRotationSpeed);
-
     }
+
+
 }
